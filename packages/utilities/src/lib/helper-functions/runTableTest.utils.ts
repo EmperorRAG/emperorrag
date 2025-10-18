ed271464bb7fd80f2c95204ca1637c262247fbd0
EmperorRAG <mrbrewermj@gmail.com>
2025-10-18 16:16:13 +0000

    refactor: update testing framework to Vitest and adjust Vite configuration for utilities package

--- packages/utilities/src/lib/helper-functions/runTableTest.utils.ts
+++ packages/utilities/src/lib/helper-functions/runTableTest.utils.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@jest/globals';
+import { test, expect } from 'vitest';
 import { pipe } from 'effect';
 import { forEach } from 'effect/Array';
 
--- packages/utilities/vite.config.ts
+++ packages/utilities/vite.config.ts
@@ -2,13 +2,13 @@
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 import * as path from 'path';
-import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
 
 export default defineConfig({
   root: __dirname,
   cacheDir: '../node_modules/.vite/utilities',
   plugins: [
-    nxViteTsPaths(),
+    nxCopyAssetsPlugin(['*.md', 'package.json']),
     dts({
       entryRoot: 'src',
       tsconfigPath: path.join(__dirname, 'tsconfig.lib.json'),
@@ -17,7 +17,7 @@ export default defineConfig({
   // Configuration for building your library.
   // See: https://vitejs.dev/guide/build.html#library-mode
   build: {
-    outDir: '../dist/packages/utilities',
+    outDir: './dist',
     reportCompressedSize: true,
     commonjsOptions: {
       transformMixedEsModules: true,
@@ -25,7 +25,7 @@ export default defineConfig({
     lib: {
       // Could also be a dictionary or array of multiple entry points.
       entry: 'src/index.ts',
-      name: 'utilities',
+      name: '@emperorrag/utilities',
       fileName: 'index',
       // Change this to the formats you want to support.
       // Don't forget to update your package.json as well.
@@ -33,13 +33,14 @@ export default defineConfig({
     },
     rollupOptions: {
       // External packages that should not be bundled into your library.
-      external: ['tslib'],
+      external: ['tslib', 'effect'],
+
     },
   },
   test: {
     globals: true,
     cache: {
-      dir: '../node_modules/.vitest/packages/utilities',
+      dir: 'node_modules/.vitest/',
     },
     environment: 'node',
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
