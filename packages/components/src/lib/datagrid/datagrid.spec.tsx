903716289637c9b834db9ca452bc4b5cc9c333f8
EmperorRAG <mrbrewermj@gmail.com>
2025-10-27 20:28:27 +0200

    feat(components): initialize components library with basic structure and Datagrid component
    - Added tsconfig.json for components library.
    - Created README.md for components library.
    - Added package.json with library metadata and configurations.
    - Implemented Datagrid component with basic styling and structure.
    - Added unit tests for Datagrid component.
    - Configured TypeScript settings for library and tests.
    - Set up Vite configuration for building and testing the library.

---
+++ packages/components/src/lib/datagrid/datagrid.spec.tsx
@@ -0,0 +1,12 @@
+import { render } from '@testing-library/react';
+
+import Datagrid from './datagrid';
+
+describe('Datagrid', () => {
+
+  it('should render successfully', () => {
+    const { baseElement } = render(<Datagrid />);
+    expect(baseElement).toBeTruthy();
+  });
+
+});
---
+++ packages/components/src/lib/datagrid/datagrid.tsx
@@ -0,0 +1,15 @@
+
+import styles from './datagrid.module.css';
+
+export function Datagrid() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Datagrid!</h1>
+    </div>
+  );
+}
+
+export default Datagrid;
+
+
+
