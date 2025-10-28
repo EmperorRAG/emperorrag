a1ba6d561437a94ead1df96b6813cffeb0e33a2a
EmperorRAG <mrbrewermj@gmail.com>
2025-10-28 16:11:05 +0200

    feat(admin): add initial Admin page with styling

---
+++ apps/better-auth-next-js-frontend/src/app/admin/page.tsx
@@ -0,0 +1,14 @@
+
+import styles from './page.module.css';
+
+export default function Admin() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Admin!</h1>
+      
+    </div>
+  );
+};
+
+
+
