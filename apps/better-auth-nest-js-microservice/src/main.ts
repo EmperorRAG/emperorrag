0478fd22b2292acb94a7d0770c0dc20c3a6c56e7
EmperorRAG <mrbrewermj@gmail.com>
2025-11-05 20:32:14 +0200

    feat: integrate Swagger for API documentation setup

--- apps/better-auth-nest-js-microservice/src/main.ts
+++ apps/better-auth-nest-js-microservice/src/main.ts
@@ -6,6 +6,7 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   // Disable body parser to let Better Auth handle it
@@ -16,6 +17,14 @@ async function bootstrap() {
 
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, documentFactory);
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
