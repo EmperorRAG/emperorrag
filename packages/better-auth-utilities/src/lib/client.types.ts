e387ed5fbc324ab3ab6a35ab0d50bd305985e5f0
EmperorRAG <mrbrewermj@gmail.com>
2025-11-14 15:18:17 +0200

    refactor: add type helper to extract API method keys from AuthClient, excluding reserved properties

--- packages/better-auth-utilities/src/lib/client.types.ts
+++ packages/better-auth-utilities/src/lib/client.types.ts
@@ -1,5 +1,7 @@
 import { createAuthClient } from 'better-auth/client';
 
+type AuthClientReservedKey = '$fetch' | '$store' | '$ERROR_CODES' | '$Infer';
+
 /**
  * Type helper to infer the full client type including all plugins.
  *
@@ -9,4 +11,20 @@ import { createAuthClient } from 'better-auth/client';
  * export type AuthClient = AuthClientFor<typeof authClient>;
  * ```
  */
-export type AuthClientFor<T extends ReturnType<typeof createAuthClient>> = T;
+export type AuthClientFor<T extends ReturnType<typeof createAuthClient> = ReturnType<typeof createAuthClient>> = T;
+
+/**
+ * Type helper to extract API method keys from an AuthClient type, excluding reserved internal properties.
+ *
+ * @description Filters out Better Auth internal keys ($fetch, $store, $ERROR_CODES, $Infer) to return only callable API methods.
+ *
+ * @example
+ * ```typescript
+ * type MyClientKeys = AuthClientApiKeyFor<typeof authClient>;
+ * // 'signIn' | 'signOut' | 'session' | 'signUp' | ... (no $ prefixed keys)
+ * ```
+ */
+export type AuthClientApiKeyFor<T extends AuthClientFor<ReturnType<typeof createAuthClient>> = AuthClientFor<ReturnType<typeof createAuthClient>>> = Omit<
+	keyof AuthClientFor<T>,
+	AuthClientReservedKey
+>;
