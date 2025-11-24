d519e46d2152c10edfb5a96ee52fc4c859851f7f
EmperorRAG <mrbrewermj@gmail.com>
2025-11-24 17:32:32 +0200

    feat(auth): add core client and server exports for better-auth-utilities package

---
+++ packages/better-auth-utilities/src/lib/client/index.ts
@@ -0,0 +1,3 @@
+export * from './client.js';
+export * from './client.types.js';
+export * from './client.constants.js';
---
+++ packages/better-auth-utilities/src/lib/core/index.ts
@@ -0,0 +1,5 @@
+export * from './account/index.js';
+export * from './email/index.js';
+export * from './oauth/index.js';
+export * from './session/index.js';
+export * from './user/index.js';
