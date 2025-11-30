7c0c783c70b776ffd6a34c02052651e3bb7086af
EmperorRAG <mrbrewermj@gmail.com>
2025-11-30 16:27:37 +0200

    chore: add externalization utilities and configuration for Rollup

--- packages/utilities/vite.config.ts
+++ packages/utilities/vite.config.ts
@@ -1,53 +1,52 @@
-/// <reference types='vitest' />
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 import * as path from 'path';
+// import * as fs from 'fs';
 import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
+import externalsJson from './externals.json' with { type: 'json' };
+import { externalizePackages, toExternalizeConfig } from './externalize-packages.js';
 
 export default defineConfig(() => ({
-  root: __dirname,
-  cacheDir: '../../node_modules/.vite/packages/utilities',
-  plugins: [
-    nxCopyAssetsPlugin(['*.md', 'package.json']),
-    dts({ entryRoot: './src', tsconfigPath: path.join(__dirname, 'tsconfig.lib.json') })
-  ],
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [ nxViteTsPaths() ],
-  // },
-  // Configuration for building your library.
-  // See: https://vitejs.dev/guide/build.html#library-mode
-  build: {
-    outDir: './dist',
-    emptyOutDir: true,
-    reportCompressedSize: true,
-    commonjsOptions: {
-      transformMixedEsModules: true,
-    },
-    lib: {
-      // Could also be a dictionary or array of multiple entry points.
-      entry: './src/index.ts',
-      name: '@emperorrag/utilities',
-      fileName: 'index',
-      // Change this to the formats you want to support.
-      // Don't forget to update your package.json as well.
-      formats: ['es' as const]
-    },
-    rollupOptions: {
-      // External packages that should not be bundled into your library.
-      external: []
-    },
-  },
-  test: {
-    name: 'utilities',
-    watch: false,
-    globals: true,
-    environment: 'node',
-    include: ['{src,tests}/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    reporters: ['default'],
-    coverage: {
-      reportsDirectory: './test-output/vitest/coverage',
-      provider: 'v8' as const,
-    }
-  },
+	root: __dirname,
+	cacheDir: '../../node_modules/.vite/packages/utilities',
+	plugins: [
+		nxCopyAssetsPlugin(['*.md', 'package.json']),
+		dts({
+			entryRoot: './src',
+			tsconfigPath: path.join(__dirname, 'tsconfig.lib.json'),
+			rollupTypes: false,
+			staticImport: true,
+		}),
+	],
+	// Uncomment this if you are using workers.
+	// worker: {
+	//  plugins: [ nxViteTsPaths() ],
+	// },
+	// Configuration for building your library.
+	// See: https://vitejs.dev/guide/build.html#library-mode
+	build: {
+		outDir: './dist',
+		emptyOutDir: true,
+		reportCompressedSize: true,
+		commonjsOptions: {
+			transformMixedEsModules: true,
+		},
+		lib: {
+			// Could also be a dictionary or array of multiple entry points.
+			entry: {
+				utilities: './src/utilities.ts',
+				'lib/helper-functions/helperFunctions': './src/lib/helper-functions/helperFunctions.ts',
+				'lib/types/types': './src/lib/types/types.ts',
+				'lib/helper-functions/object.utils': './src/lib/helper-functions/object.utils.ts',
+			},
+			name: '@emperorrag/utilities',
+			// Change this to the formats you want to support.
+			// Don't forget to update your package.json as well.
+			formats: ['es' as const],
+		},
+		rollupOptions: {
+			// External packages that should not be bundled into your library.
+			external: externalizePackages(toExternalizeConfig(externalsJson)),
+		},
+	},
 }));
