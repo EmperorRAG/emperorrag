3092285f0b43484801488b7f0f4e26882e49c66f
EmperorRAG <mrbrewermj@gmail.com>
2025-12-01 18:52:09 +0200

    feat: simplify exports in better-auth-utilities.barrel.ts for improved clarity

--- packages/better-auth-utilities/src/better-auth-utilities.barrel.ts
+++ packages/better-auth-utilities/src/better-auth-utilities.barrel.ts
@@ -3,31 +3,4 @@
  * @description Public API for the better-auth-utilities library.
  */
 
-export { createAuthServer, createClientConfig, createServerConfig, defineConfig, createAuthClient } from './lib/lib.barrel';
-
-export type {
-	AuthProvider,
-	AvailablePlugins,
-	BetterAuthConfig,
-	ClientConfig,
-	DEFAULT_CLIENT_CONFIG,
-	DEFAULT_SERVER_CONFIG,
-	AuthClientApiEndpointFor,
-	AuthClientApiEndpointKeyFor,
-	AuthClientApiFor,
-	AuthClientApiKeyFor,
-	AuthClientFor,
-	AuthClientSignInFor,
-	AuthClientSignUpFor,
-	ServerConfig,
-	AuthServerFor,
-	AuthServerApiKeyFor,
-	AuthServerApiFor,
-	AuthServerApiEndpointKeyFor,
-	AuthServerApiEndpointFor,
-	AuthServerSessionFor,
-	AuthServerSessionUserFor,
-	AuthServerSessionUserSessionFor,
-	AuthServerSignInFor,
-	AuthServerSignUpFor,
-} from './lib/lib.barrel';
+export * from './lib/lib.barrel';
