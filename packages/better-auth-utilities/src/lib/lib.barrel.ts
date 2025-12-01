523921b783225e6d00e2c45c93496f0a014b9ed4
EmperorRAG <mrbrewermj@gmail.com>
2025-12-01 18:28:03 +0200

    feat: update externals.json to include http and https, and refactor lib.barrel.ts for improved exports

--- packages/better-auth-utilities/src/lib/lib.barrel.ts
+++ packages/better-auth-utilities/src/lib/lib.barrel.ts
@@ -3,44 +3,8 @@
  * @description Main export file for better-auth-utilities library
  */
 
-// Export configuration utilities
-export { defineConfig, createClientConfig, createServerConfig } from './config/config.service';
-export { DEFAULT_CLIENT_CONFIG, DEFAULT_SERVER_CONFIG } from './config/config.constants';
-export type {
-	AuthProvider,
-	AvailablePlugins,
-	BetterAuthConfig,
-	ClientConfig,
-	OAuthProviderId,
-	PluginConfigRegistry,
-	ServerConfig,
-} from './config/config.types';
-
-// Export server instance creation
-export { createAuthServer } from './server/server.service';
-
-// Export server types
-export type {
-	AuthServerFor,
-	AuthServerApiKeyFor,
-	AuthServerApiFor,
-	AuthServerApiEndpointKeyFor,
-	AuthServerApiEndpointFor,
-	AuthServerSessionFor,
-	AuthServerSessionUserFor,
-	AuthServerSessionUserSessionFor,
-	AuthServerSignInFor,
-	AuthServerSignUpFor,
-} from './server/server.types';
-
-// Export client instance creation
-export { createAuthClient } from './client/client.service';
-export type {
-	AuthClientApiEndpointFor,
-	AuthClientApiEndpointKeyFor,
-	AuthClientApiFor,
-	AuthClientApiKeyFor,
-	AuthClientFor,
-	AuthClientSignInFor,
-	AuthClientSignUpFor,
-} from './client/client.types';
+export * from './client/client.barrel';
+export * from './config/config.barrel';
+export * from './core/core.barrel';
+export * from './server/server.barrel';
+export * from './test/setup-test-env';
