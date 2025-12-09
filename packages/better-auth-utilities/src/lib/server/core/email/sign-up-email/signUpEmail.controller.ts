5fb5b9d979aabf0be1ae8ddc33ece92cc9964e15
EmperorRAG <mrbrewermj@gmail.com>
2025-12-09 18:24:38 +0200

    feat: implement dynamic Zod schema creation for signUpEmail parameters based on AuthServer configuration

--- packages/better-auth-utilities/src/lib/server/core/email/sign-up-email/signUpEmail.controller.ts
+++ packages/better-auth-utilities/src/lib/server/core/email/sign-up-email/signUpEmail.controller.ts
@@ -1,14 +1,18 @@
 import * as Effect from 'effect/Effect';
-import { signUpEmailServerParamsSchema } from './signUpEmail.schema';
+import { createSignUpEmailServerParamsSchema } from './signUpEmail.schema';
 import type { AuthServerFor } from '../../../server.types';
 import type { AuthServerApiSignUpEmailParamsFor, signUpEmailPropsFor } from './signUpEmail.types';
 import { EmailAuthServerInputError } from '../shared/email.error';
 import { signUpEmailServerService } from './signUpEmail.service';
+import { EmailAuthServerServiceTag } from '../shared/email.service';
 
 export const signUpEmailServerController: signUpEmailPropsFor = <T extends AuthServerFor = AuthServerFor>(params: AuthServerApiSignUpEmailParamsFor<T>) =>
 	Effect.gen(function* (_) {
+		const { authServer } = yield* _(EmailAuthServerServiceTag);
+		const schema = yield* _(createSignUpEmailServerParamsSchema(authServer));
+
 		// 1) Validate params input with Zod
-		const parsed = signUpEmailServerParamsSchema.safeParse(params);
+		const parsed = schema.safeParse(params);
 
 		if (!parsed.success) {
 			// You could format Zod's error more nicely here
