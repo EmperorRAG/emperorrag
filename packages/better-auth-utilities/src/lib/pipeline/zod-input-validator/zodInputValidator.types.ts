0ea01b6dbb41327759173f7f6ecc0edbc3a20328
EmperorRAG <mrbrewermj@gmail.com>
2025-12-12 17:45:52 +0200

    refactor: change ZodInputValidatorProps from interface to type for improved consistency

--- packages/better-auth-utilities/src/lib/pipeline/zod-input-validator/zodInputValidator.types.ts
+++ packages/better-auth-utilities/src/lib/pipeline/zod-input-validator/zodInputValidator.types.ts
@@ -3,8 +3,6 @@ import type { z } from 'zod';
 import type { PipelineContext } from '../../context/pipeline.context';
 import type { AuthServerInputError } from '../../errors/authServer.error';
 
-export interface ZodInputValidatorProps {
-	<R>(
-		schemaEffect: Effect.Effect<z.ZodType, unknown, R>
-	): <T>(typeGuard: (value: unknown) => value is T) => (input: unknown) => Effect.Effect<T, AuthServerInputError, R | PipelineContext>;
-}
+export type ZodInputValidatorProps = <R>(
+	schemaEffect: Effect.Effect<z.ZodType, unknown, R>
+) => <T>(typeGuard: (value: unknown) => value is T) => (input: unknown) => Effect.Effect<T, AuthServerInputError, R | PipelineContext>;
